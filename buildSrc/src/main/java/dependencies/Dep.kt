455ff3807383e542c010dc4417cb223daaa916d9
batch <baaaakkrad@gmail.com>
2021-01-02 21:57:02 +0900

    git rm

--- buildSrc/src/main/java/dependencies/Dep.kt
+++
@@ -1,9 +0,0 @@
-package dependencies
-
-object Dep {
-    object AndroidX {
-        val appCompat = "androidx.appcompat:appcompat:1.2.0"
-        val constraint = "androidx.constraintlayout:constraintlayout:2.0.4"
-        val design = "com.google.android.material:material:1.2.1"
-    }
-}
---
+++ buildSrc/src/main/kotlin/dependencies/Dep.kt
@@ -0,0 +1,9 @@
+package dependencies
+
+object Dep {
+    object AndroidX {
+        const val appCompat = "androidx.appcompat:appcompat:1.2.0"
+        const val constraint = "androidx.constraintlayout:constraintlayout:2.0.4"
+        const val design = "com.google.android.material:material:1.2.1"
+    }
+}
