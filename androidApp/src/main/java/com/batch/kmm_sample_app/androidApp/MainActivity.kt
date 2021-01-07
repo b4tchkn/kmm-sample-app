dcd14192185b3f4f76701a978e62a3f65b878fe9
batch <baaaakkrad@gmail.com>
2021-01-07 23:45:37 +0900

    add searchView

--- androidApp/src/main/java/com/batch/kmm_sample_app/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/batch/kmm_sample_app/androidApp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.batch.kmm_sample_app.androidApp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.lifecycleScope
 import com.batch.kmm_sample_app.androidApp.databinding.ActivityMainBinding
 import com.batch.kmm_sample_app.shared.data.local.DatabaseDriverFactory
@@ -27,8 +29,23 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.searchActress("佐々木")
+        binding.mainToolbar.inflateMenu(R.menu.search)
+        val searchView =
+            binding.mainToolbar.menu.findItem(R.id.menu_search).actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(p0: String?): Boolean {
+                Log.d("activityMain", "Change${p0}")
+                return true
+            }
 
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                if (!p0.isNullOrEmpty()) {
+                    viewModel.searchActress(p0)
+                }
+                return true
+            }
+        })
         lifecycleScope.launchWhenStarted {
             viewModel.actresses.collect {
                 binding.textView.text = it.toString()
